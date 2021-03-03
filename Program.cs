1b1a816bac41c9b91de9850c5ca1342ca3977dbe
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-03 08:53:21 -0300

    Arte Ascii

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Atividade3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("//\\//\\");
+            Console.WriteLine("((ovo))");
+            Console.WriteLine("():::()");
+            Console.WriteLine("  VVV");
+        }
+    }
+}
